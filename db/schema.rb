5e40fc1362f7aa2c0be52b300483bce9d54ac19b
murasame <murasame.usa@gmail.com>
2024-12-16 17:54:29 +0900

    :new:イベントグループモデル作成＋マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_13_043211) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_083958) do
+  create_table "event_groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.text "image_url", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_event_groups_on_name"
+    t.index ["user_id"], name: "index_event_groups_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "display_name"
     t.string "profile_picture_url"
@@ -23,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_13_043211) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "event_groups", "users"
 end
