a2c78fdfd03271b3ad636050b68cee728b7a769c
murasame <murasame.usa@gmail.com>
2024-12-23 17:56:43 +0900

    :new:イベントグループに対するCRUD処理のロジックを追加し、関連するバリデーションを実装

--- app/models/event_group.rb
+++ app/models/event_group.rb
@@ -1,2 +1,7 @@
 class EventGroup < ApplicationRecord
+  before_validation :set_default_image_url
+
+  def set_default_image_url
+    self.image_url ||= 'https://pbs.twimg.com/profile_banners/893121515847155712/1734629052/1500x500'
+  end
 end
