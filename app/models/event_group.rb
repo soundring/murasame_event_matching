e14d0a80f8cbb50f03f3caedb62360ef386511cf
murasame <murasame.usa@gmail.com>
2024-12-23 18:19:46 +0900

    :wrench:バリデーション追加
    名前必須
    
    画像の形式とかのバリデーションはActive Storage使う際に

--- app/models/event_group.rb
+++ app/models/event_group.rb
@@ -3,6 +3,8 @@ class EventGroup < ApplicationRecord
 
   before_validation :set_default_image_url
 
+  validates :name, presence: true
+
   def set_default_image_url
     self.image_url ||= 'https://pbs.twimg.com/profile_banners/893121515847155712/1734629052/1500x500'
   end
