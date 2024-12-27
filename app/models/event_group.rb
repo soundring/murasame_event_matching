ecd6d77be0ac36b8045923e09f2b8bb1460d7c00
murasame <murasame.usa@gmail.com>
2024-12-27 18:31:26 +0900

    :wrench:イベントグループモデルにメソッド追加
    作成者か確認
    管理者か確認
    作成後に自動で管理者に追加

--- app/models/event_group.rb
+++ app/models/event_group.rb
@@ -4,10 +4,22 @@ class EventGroup < ApplicationRecord
   has_many :admin_users, through: :event_group_admins, source: :user # 管理者
 
   before_validation :set_default_image_url
+  after_create :add_creator_as_admin
 
   validates :name, presence: true
 
+  def admin?(user)
+    return false unless user
+    admin_users.include?(user)
+  end
+
+  private
+
   def set_default_image_url
     self.image_url ||= 'https://pbs.twimg.com/profile_banners/893121515847155712/1734629052/1500x500'
+  end
+
+  def add_creator_as_admin
+    event_group_admins.create(user: user)
   end
 end
--- spec/models/event_group_spec.rb
+++ spec/models/event_group_spec.rb
@@ -1,30 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe EventGroup, type: :model do
+  let(:user) { create(:user) }
+  let(:admin_user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let!(:event_group) { create(:event_group, user: user) }
+
   describe 'バリデーション' do
     it 'nameがあれば有効であること' do
-      event_group = build(:event_group)
-      expect(event_group).to be_valid
+      valid_event_group = build(:event_group, user: user)
+      expect(valid_event_group).to be_valid
     end
 
     it 'nameがなければ無効であること' do
-      event_group = build(:event_group, name: nil)
-      event_group.valid?
-      expect(event_group.errors[:name]).to include("can't be blank")
+      invalid_event_group = build(:event_group, name: nil, user: user)
+      expect(invalid_event_group).not_to be_valid
+      expect(invalid_event_group.errors[:name]).to include("can't be blank")
     end
   end
 
-  describe 'デフォルト画像URLの設定' do
+  describe 'callbacks' do
     it 'image_urlがnilの場合、デフォルトのURLが設定されること' do
-      event_group = EventGroup.new(name: 'Test Group', user: create(:user))
-      event_group.valid?
+      event_group = EventGroup.create(name: 'Test Group', user: user)
       expect(event_group.image_url).to eq('https://pbs.twimg.com/profile_banners/893121515847155712/1734629052/1500x500')
     end
 
-    it 'image_urlが設定されている場合、デフォルトのURLが設定されないこと' do
-      event_group = EventGroup.new(name: 'Test Group', image_url: 'https://example.com/image.jpg', user: create(:user))
-      event_group.valid?
-      expect(event_group.image_url).to eq('https://example.com/image.jpg')
+    it 'image_urlが設定されている場合、デフォルトのURLを上書きしないこと' do
+      custom_url = 'http://example.com/custom_image.jpg'
+      event_group = EventGroup.create(name: 'Test Group',  user: user, image_url: custom_url)
+      expect(event_group.image_url).to eq(custom_url)
+    end
+
+    it '作成時に作成者が管理者として追加されること' do
+      event_group = EventGroup.create(name: 'テストグループ', user: user)
+      expect(event_group.admin_users).to include(user)
+    end
+  end
+
+  describe '#admin?' do
+    it '管理者ユーザーが含まれる場合、trueを返すこと' do
+      create(:event_group_admin, user: admin_user, event_group: event_group)
+
+      expect(event_group.admin?(admin_user)).to be true
+    end
+
+    it '管理者ユーザーでない場合、falseを返すこと' do
+      expect(event_group.admin?(other_user)).to be false
+    end
+
+    it 'ユーザーが渡されなかった場合、falseを返すこと' do
+      expect(event_group.admin?(nil)).to be false
     end
   end
 end
