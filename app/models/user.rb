b730930a27f8e10d3eea78a701f4dc9d43827f66
murasame <murasame.usa@gmail.com>
2025-02-03 17:58:53 +0900

    :wrench:アソシエーション漏れ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :validatable
 
   has_many :event_groups # 作成したグループ
+  has_many :event_group_admins
   has_many :administered_groups, through: :event_group_admins, source: :event_group # 管理者になってるグループ
   has_many :events, as: :eventable
 end
