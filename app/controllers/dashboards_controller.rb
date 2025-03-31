d83e3c523a82bcd017aca02a82d3f91cb36b324d
murasame <murasame.usa@gmail.com>
2025-03-31 23:46:36 +0900

    :sparkles: N+1対応

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,10 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
+  # TODO: カウントに関しては、counter_cacheを使うようにする
   def show
     @participating_events = current_user.participating_events
+                                      .with_attached_image
                                       .includes(:eventable, eventable: :events)
                                       .published
                                       .now_than_after
@@ -10,12 +12,14 @@ class DashboardsController < ApplicationController
                                       .limit(5)
 
     @administered_groups = current_user.administered_groups
+                                     .with_attached_image
                                      .includes(:events)
                                      .order(created_at: :asc)
                                      .limit(5)
 
     # TODO: ユーザーイベントも含める
-    @administered_events = Event.preload(:eventable)
+    @administered_events = Event.with_attached_image
+                            .preload(:eventable)
                             .where(eventable_type: 'EventGroup',
                                    eventable_id: current_user.administered_group_ids)
                             .now_than_after
